75a0d998ecb19406526389174326a72881385952
Vasiliy-Gorbunov <79896130110@yandex.ru>
2023-01-02 22:56:18 +0300

    OOP2_task1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car("", "Granta", 1.7, "Жёлтый", 2015, "Россия");
-        Car audi = new Car("Audi", null, 3.0, "Чёрный", 2020, "Германия");
-        Car bmw = new Car("BMW", "Z8", -3.0, "Чёрный", 2021, "Германия");
-        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "", 2018, "Южная Корея");
-        Car hyundai = new Car("Hyundai", "Avante", 1.6, "Оранжевый", -2016, null);
+        Car lada = new Car("", "Granta", 1.7, "Жёлтый", 2015, "Россия", "Ручная", "1", "о123ее37", 6, true);
+        Car audi = new Car("Audi", null, 3.0, "Чёрный", 2020, "Германия", null, "", "", 3, false);
+        Car bmw = new Car("BMW", "Z8", -3.0, "Чёрный", 2021, "Германия", "", null, "a666ее66", 2, false);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "", 2018, "Южная Корея", "Авто", "2б", "р777ух77", 0, true);
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "Оранжевый", -2016, null, "Ручная", "", null, 6, true);
         System.out.println(lada);
         System.out.println(audi);
         System.out.println(bmw);
         System.out.println(kia);
+        System.out.println(hyundai);
+        hyundai.setActualTyre(6);
         System.out.println(hyundai);
     }
 }
---
+++ src/transport/Car.java
@@ -0,0 +1,166 @@
+package transport;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private final int productionYear;
+    private final String productionCountry;
+    private String transmission;
+    private final String bodyType;
+    private String regNumber;
+    private final int seatNumbers;
+    private boolean winterTyres;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry, String transmission, String bodyType, String regNumber, int seatNumbers, boolean winterTyres) {
+        if (brand == null || brand.isEmpty() || brand.isBlank()) {
+            brand = "default";
+        }
+        if (model == null || model.isEmpty() || model.isBlank()) {
+            model = "default";
+        }
+        if (productionCountry == null || productionCountry.isEmpty() || productionCountry.isBlank()) {
+            productionCountry = "default";
+        }
+        if (Double.compare(engineVolume, 0) == 0 || engineVolume < 0) {
+            engineVolume = 1.5;
+        }
+        if (color == null || color.isEmpty() || color.isBlank()) {
+            color = "белый";
+        }
+        if (productionYear <= 0) {
+            productionYear = 2000;
+        }
+        if (transmission == null || transmission.isEmpty() || transmission.isBlank()) {
+            transmission = "Авто";
+        }
+        if (bodyType == null || bodyType.isEmpty() || bodyType.isBlank()) {
+            bodyType = "default";
+        }
+        if (regNumber == null || regNumber.isEmpty() || regNumber.isBlank()) {
+            regNumber = "default";
+        }
+        if (seatNumbers <= 0) {
+            seatNumbers = 4;
+        }
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+        this.transmission = transmission;
+        this.bodyType = bodyType;
+        this.regNumber = regNumber;
+        this.seatNumbers = seatNumbers;
+        this.winterTyres = winterTyres;
+    }
+
+    public void setActualTyre(int month) {
+        if (month > 3 && month < 11) {
+            winterTyres = false;
+        } else {
+            winterTyres = true;
+        }
+    }
+
+    private String separator() {
+        return "=======================";
+    }
+
+    @Override
+    public String toString() {
+        String tyre = "летняя";
+        if (winterTyres) {
+            tyre = "зимняя";
+        }
+        return "Автомобиль: " + brand + " " + model + "\n " +
+                productionYear + " года выпуска" + "\n " +
+                "Страна сборки: " + productionCountry + "\n " +
+                "Цвет: " + color + "\n " +
+                "Объём двигателя: " + engineVolume + " л." + "\n " +
+                "Тип трансмиссии: " + transmission + "\n " +
+                "Тип кузова: " + bodyType + "\n " +
+                "Регистрационный номер: " + regNumber + "\n " +
+                "Количество мест: " + seatNumbers + "\n " +
+                "Резина: " + tyre + "\n" +
+                separator();
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getSeatNumbers() {
+        return seatNumbers;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        if (Double.compare(engineVolume, 0) == 0 || engineVolume < 0) {
+            engineVolume = 1.5;
+        }
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color == null || color.isEmpty() || color.isBlank()) {
+            color = "белый";
+        }
+        this.color = color;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        if (transmission == null || transmission.isEmpty() || transmission.isBlank()) {
+            transmission = "Авто";
+        }
+        this.transmission = transmission;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        if (regNumber == null || regNumber.isEmpty() || regNumber.isBlank()) {
+            regNumber = "default";
+        }
+        this.regNumber = regNumber;
+    }
+
+    public boolean isWinterTyres() {
+        return winterTyres;
+    }
+
+    public void setWinterTyres(boolean winterTyres) {
+        this.winterTyres = winterTyres;
+    }
+}
